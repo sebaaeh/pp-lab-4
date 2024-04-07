78ac0182db662cc5ff28f35bad9c1dc061693b8b
Sebastian Graczyk <seba.graczyk123@gmail.com>
2024-04-07 18:41:51 +0200

    a

--- Company.java
+++ Company.java
@@ -5,10 +5,10 @@ public class Company {
         Employee[] employees = new Employee[5]; 
 
         employees[0] = new Manager("Jan Kowalski", 5000, 0);
-        employees[1] = new Worker("Mariusz Tynmian", 3500, "Technician");
+        employees[1] = new Worker("Mariusz Tynmian", 3500, "złota rączka");
         employees[2] = new Employee("Łukasz Tytman", 2500);
-        employees[3] = new Worker("Jacek Żyznowski", 5800, "Engineer");
-        employees[4] = new Worker("Adam Trojecki", 5300, "Assistant");
+        employees[3] = new Worker("Jacek Żyznowski", 5800, "inżynier");
+        employees[4] = new Worker("Adam Trojecki", 5300, "inżynier");
 
         int nonManagersCount = 0;
         for (Employee employee : employees) {
